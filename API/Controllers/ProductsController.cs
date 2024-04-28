c773c437c365a21b6fa9a56dc452d8b44381bb39
Gee Chong Shek <geeshek95@outlook.com>
2024-04-28 12:29:15 +0100

    Created API backend skeleton: implemented product entity,db, migration, controller and service

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using API.Entities;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly ProductService _productService;
+
+    public ProductsController(ProductService productService)
+    {
+        _productService = productService;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _productService.GetProducts();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _productService.GetProductById(id);
+    }
+}
