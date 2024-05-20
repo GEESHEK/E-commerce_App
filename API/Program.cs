68f522f1aaad76d17e70e321d462c42b11d67678
Gee Chong Shek <geeshek95@outlook.com>
2024-05-20 20:19:16 +0100

    Factored the services and implemented an ApplicationServiceExtension.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,11 @@
-using API.Data;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<WatchService>();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
