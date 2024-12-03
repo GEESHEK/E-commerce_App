d8493023bfe6909632837cde512ae396c2ec5d8e
Gee Chong Shek <geeshek95@outlook.com>
2024-12-03 17:40:28 +0000

    Bug fixed, Id added to appuser, watch card can be retrieved

--- API/Entities/UserEntities/AppUser.cs
+++ API/Entities/UserEntities/AppUser.cs
@@ -4,6 +4,7 @@ namespace API.Entities.UserEntities;
 
 public class AppUser
 {
+    public int Id { get; set; }
     public string KnownAs { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
