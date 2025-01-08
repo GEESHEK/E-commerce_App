c7c9bfadcdc564e43dec793a66f078a05d01e906
Gee Chong Shek <geeshek95@outlook.com>
2025-01-08 20:08:58 +0000

    Created simple outline for accounts page

--- client/src/app/user/account-page/account-page.component.ts
+++ client/src/app/user/account-page/account-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../../models/user";
+import {AccountService} from "../../services/account.service";
 
 @Component({
   selector: 'app-account-page',
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.css']
 })
-export class AccountPageComponent {
+export class AccountPageComponent implements OnInit {
+ user: User | null  = this.accountService.currentUser$();
+ username: string | null = "";
 
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    if (this.user) {
+      this.username = this.user?.username ? this.user.username : null;
+    }
+  }
 }
